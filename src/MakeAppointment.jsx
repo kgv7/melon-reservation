1c42331c016f949c86dba40f1cf14649ddac01c2
kgv7 <kaitlyn.a.goodman@gmail.com>
2022-01-10 01:50:28 +0000

    added calendar

--- src/MakeAppointment.jsx
+++ src/MakeAppointment.jsx
@@ -1,10 +1,25 @@
-import React from "react"
+import React, {useState} from "react"
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css";
+
 
 export default function MakeAppointment() {
-
-    return(
+    const [dateRange, setDateRange] = useState([null, null]);
+    const [startDate, endDate] = dateRange;
+    return (
         <div>
-            <h1>Schedule Tasting</h1>
+            <h1>Schedule a Tasting</h1>
+            <h3>Choose a range of dates to find an available 30-minute tasting.</h3>
+            <DatePicker
+                selectsRange={true}
+                startDate={startDate}
+                endDate={endDate}
+                onChange={(update) => {
+                    setDateRange(update);
+                }}
+                withPortal
+            />
         </div>
-    )
+    );
+
 }
